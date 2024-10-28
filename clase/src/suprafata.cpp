273398718ce243a07c9ed0882d5604b3697fed40
dimu0802 <dimupurcarea0802@gmail.com>
2024-10-28 16:31:24 +0200

    fix suprafata.cpp

--- clase/src/suprafata.cpp
+++ clase/src/suprafata.cpp
@@ -2,21 +2,21 @@
 #include "../include/suprafata.h"
 
 Suprafata::Suprafata(Jucator& p1_dat, Jucator& p2_dat):
-    p1(p1_dat), p2(p2_dat){}
+    p1(p1_dat), p2(p2_dat), suprafata{0}{}
 
 void Suprafata::suprafata_noua(){
     int poz1=p1.pozitie_actuala();
     int poz2=p2.pozitie_actuala();
-    std::array<int, 12> suprafata_noua={0};
+    std::array<int, 12> suprafata_actualizata={0};
     for(int i=0; i<12; i++){
         if(i==poz1){
-            suprafata_noua[i]=1;
+            suprafata_actualizata[i]=1;
         }
         else if(i==poz2){
-            suprafata_noua[i]=2;
+            suprafata_actualizata[i]=2;
         }
     }
-    suprafata=suprafata_noua;
+    suprafata=suprafata_actualizata;
     
 }
 
@@ -43,9 +43,10 @@ void Suprafata::resetare_suprafata(){
     p1.revenire_pozitie_initiala(poz1);
     p2.revenire_pozitie_initiala(poz2);
 
-    std::array<int, 12> suprafata_noua={0};
-    suprafata_noua[poz1]=1;
-    suprafata_noua[poz2]=2;
+    std::array<int, 12> suprafata_resetata={0};
+    suprafata_resetata[poz1]=1;
+    suprafata_resetata[poz2]=2;
+    suprafata=suprafata_resetata;
 
 }
 
