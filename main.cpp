18ca56a7c53adf7310bcc02c7db6a1b41f4706d1
dimu0802 <dimupurcarea0802@gmail.com>
2024-11-11 19:01:58 +0200

    tratare input necorespunzator + if-uri asemanatoare in clasa joc 2

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 
 int main(){
+    std::cout<<"\n\nDupa ce ti-ai dedicat o viata intreaga acestui sport, ai reusit sa ajungi in finala olimpica! Felicitari!\nAcum urmeaza cel mai important meci al tau, cel care te poate propulsa in istoria sportului tau. Crezi ca faci fata provocarii?";
     Joc joc("Jucator 1", "Jucator 2", 4, 7);
     joc.start();
 
