cd2d82603053e48270576446a5321db010c79dfc
dimu0802 <dimupurcarea0802@gmail.com>
2024-11-13 16:32:23 +0200

    upfate if-uri 4 + reguli

--- main.cpp
+++ main.cpp
@@ -23,12 +23,12 @@ int main(){
              <<"In momentul in care jucatorii ajung unul langa celalalt, urmeaza un moment de lupta.\n"
              <<"Actiuni posibile: ATAC: tasta 1; OCOLIRE: tasta 2; PARADA: tasta 3\n\n"
              <<"Situatii posibile:\n"
-             <<"1) ATAC-ATAC: puncteaza ambii"
-             <<"2) OCOLIRE-OCOLIRE: puncteaza ambii"
-             <<"3) PARADA-PARADA: ambii se retrag"
-             <<"4) ATAC-PARADA: puncteaza PARADA"
-             <<"5) ATAC-OCOLIRE: puncteaza ATAC"
-             <<"6) OCOLIRE-PARADA: puncteaza OCOLIRE"
+             <<"1) ATAC-ATAC: puncteaza ambii\n"
+             <<"2) OCOLIRE-OCOLIRE: puncteaza ambii\n"
+             <<"3) PARADA-PARADA: ambii se retrag\n"
+             <<"4) ATAC-PARADA: puncteaza PARADA\n"
+             <<"5) ATAC-OCOLIRE: puncteaza ATAC\n"
+             <<"6) OCOLIRE-PARADA: puncteaza OCOLIRE\n"
              <<"===================================\n";
     Joc joc("Jucator 1", "Jucator 2", 4, 7);
     joc.start();
