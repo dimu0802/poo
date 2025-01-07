c61f11a1c2f1132c65cfdca9b1f186d0789a6323
dimu0802 <dimupurcarea0802@gmail.com>
2025-01-07 14:10:48 +0200

    problema tastatura.txt3

--- main.cpp
+++ main.cpp
@@ -11,25 +11,25 @@
 #include "joc_sabie.h"
 #include "joc_spada.h"
 
-int main() {
+int main(){
 
-    try {
-        std::cout << "\n\n===================================\n"
-                  << "FENCING DUEL-OLYMPIC GLORY\n"
-                  << "Format: Player vs Player\n"
-                  << "===================================\n\n"
-                  << "\nDupa ce ti-ai dedicat o viata intreaga\n"
-                  << "acestui sport, ai reusit sa ajungi in finala olimpica! Felicitari!\n"
-                  << "Acum urmeaza cel mai important meci al tau, cel care te poate propulsa\n"
-                  << "in istoria sportului tau. Crezi ca faci fata provocarii?\n\n"
-                  << "===================================\n";
+    try{
+        std::cout<<"\n\n===================================\n"
+                <<"FENCING DUEL-OLYMPIC GLORY\n"
+                <<"Format: Player vs Player\n"
+                <<"===================================\n\n"
+                <<"\nDupa ce ti-ai dedicat o viata intreaga\n"
+                <<"acestui sport, ai reusit sa ajungi in finala olimpica! Felicitari!\n"
+                <<"Acum urmeaza cel mai important meci al tau, cel care te poate propulsa\n"
+                <<"in istoria sportului tau. Crezi ca faci fata provocarii?\n\n"
+                <<"===================================\n";
 
         Lobby lobby;
         lobby.start_joc("reguli/floreta.txt", "reguli/sabie.txt", "reguli/spada.txt");
-    } catch (const ExceptieInputInvalid& ex) {
-        std::cerr << "Eroare de input: " << ex.what() << std::endl;
-    } catch (const std::exception& ex) {
-        std::cerr << "Eroare generala: " << ex.what() << std::endl;
+    }catch(const ExceptieInputInvalid& ex){
+        std::cerr<<"Eroare de input: "<<ex.what()<<std::endl;
+    }catch(const std::exception& ex){
+        std::cerr<<"Eroare generala: "<<ex.what()<<std::endl;
     }
 
     return 0;
